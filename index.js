d5edb39fae784c93e08c1005f45d6545c8feae5e
premnath <premnathkulal1998@gmail.com>
2022-09-20 19:57:55 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
+function initializeApp() {
+  const welcomeText = [
+    "Hello",
+    "नमस्ते 🙏",
+    "ನಮಸ್ಕಾರ 🙏",
+    "ഹലോ 🙏",
+    "வணக்கம் 🙏",
+    "こんにちは 🙇‍♂️",
+    "你好",
+  ];
+  const welcomeTextDocument = document.getElementById("greet-message");
+  let count = 0;
+
+  setInterval(() => {
+    welcomeTextDocument.classList.add("fade");
+    setTimeout(() => {
+      if (count >= welcomeText.length) {
+        count = 0;
+      }
+      welcomeTextDocument.innerHTML = welcomeText[count];
+      welcomeTextDocument.classList.remove("fade");
+    }, 500);
+
+    count += 1;
+  }, 5000);
+}
+
 const menuToggler = document.getElementById("toggler-button");
 menuToggler.addEventListener("click", () => {
   const isMenuOpen = document
     .getElementsByClassName("navbar-nav")[0]
     .classList.contains("show");
-  !isMenuOpen
-    ? document.getElementsByClassName("navbar-nav")[0].classList.add("show")
-    : document.getElementsByClassName("navbar-nav")[0].classList.remove("show");
+  if (!isMenuOpen) {
+    document.getElementsByClassName("navbar-nav")[0].classList.add("show");
+    document.getElementById("menu-icon").style.display = "none";
+    document.getElementById("xbar-icon").style.display = "block";
+  } else {
+    document.getElementById("menu-icon").style.display = "block";
+    document.getElementById("xbar-icon").style.display = "none";
+    document.getElementsByClassName("navbar-nav")[0].classList.remove("show");
+  }
 });
+
+initializeApp();
